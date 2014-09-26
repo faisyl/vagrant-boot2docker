c8cd1ba5742b8e81c36f01bc70feda7703b820e4
Faisal <faisal.is@gmail.com>
2014-09-26 17:28:50 +0530

    Kill off boot2dockers udhcp instance
    If using a static IP, kill off the udhcp instance so this ip address
    sticks.

--- lib/vagrant-boot2docker/cap/configure_networks.rb
+++ lib/vagrant-boot2docker/cap/configure_networks.rb
@@ -10,6 +10,8 @@ module VagrantPlugins
               if n.key?("ip")
 	              ifc = "/sbin/ifconfig eth#{n[:interface]}"
 	              broadcast = (IPAddr.new(n[:ip]) | (~ IPAddr.new(n[:netmask]))).to_s
+                # First kill the udhcp client for this interface
+                comm.sudo("kill `ps -ef |grep dhcp | grep eth#{n[:interface]} | grep -v grep | tr -s ' ' | cut -d' ' -f2`")
 	              comm.sudo("#{ifc} down")
 	              comm.sudo("#{ifc} #{n[:ip]} netmask #{n[:netmask]} broadcast #{broadcast}")
 	              comm.sudo("#{ifc} up")
--- lib/vagrant-boot2docker/version.rb
+++ lib/vagrant-boot2docker/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
 	module Boot2docker
-		VERSION = "0.0.4"
+		VERSION = "0.0.5"
 	end
 end
