75eb0db7546b9cb4973ee15c2f81810a876265e6
Faisal <faisal.is@gmail.com>
2014-09-26 17:46:51 +0530

    Attempt alternate approach
    Try killing udhcp clinet another way

--- lib/vagrant-boot2docker/cap/configure_networks.rb
+++ lib/vagrant-boot2docker/cap/configure_networks.rb
@@ -9,9 +9,10 @@ module VagrantPlugins
             networks.each do |n|
               if n.key?("ip")
 	              ifc = "/sbin/ifconfig eth#{n[:interface]}"
+                iface = "eth#{n[:interface]}"
 	              broadcast = (IPAddr.new(n[:ip]) | (~ IPAddr.new(n[:netmask]))).to_s
                 # First kill the udhcp client for this interface
-                comm.sudo("kill `ps -ef |grep dhcp | grep eth#{n[:interface]} | grep -v grep | tr -s ' ' | cut -d' ' -f2`")
+                comm.sudo("ps -ef |grep dhcp | grep #{iface} | grep -v grep | tr -s ' ' | cut -d' ' -f2| xargs kill")
 	              comm.sudo("#{ifc} down")
 	              comm.sudo("#{ifc} #{n[:ip]} netmask #{n[:netmask]} broadcast #{broadcast}")
 	              comm.sudo("#{ifc} up")
